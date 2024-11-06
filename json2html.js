31388a218419e1e39dfc14fa20b0ac8cc89ad156
harshavardhanreddy085 <125790566+harshavardhanreddy085@users.noreply.github.com>
2024-11-06 14:59:33 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,31 +1,33 @@
+
 export default function json2html(data) {
-    // Get all unique columns from the data
-    const columns = new Set();
-    data.forEach(obj => {
-        Object.keys(obj).forEach(key => columns.add(key));
-    });
-    
-    // Convert columns set to array
-    const columnArray = Array.from(columns);
-    
-    // Create table header
-    let html = '<table data-user="harshavardhanreddy9558@gmail.com">\n';
-    html += '  <thead>\n    <tr>';
-    columnArray.forEach(column => {
-        html += <th>${column}</th>;
-    });
-    html += '</tr>\n  </thead>\n';
-    
-    // Create table body
-    html += '  <tbody>\n';
-    data.forEach(row => {
-        html += '    <tr>';
-        columnArray.forEach(column => {
-            html += <td>${row[column] || ''}</td>;
-        });
-        html += '</tr>\n';
-    });
-    html += '  </tbody>\n</table>';
-    
-    return html;
+  let html = `
+    <table data-user="harshavardhanreddy9558@gmail.com">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  data.forEach((item) => {
+    html += `<tr>
+               <td>${item.Name}</td>
+               <td>${item.Age}</td>`;
+    if (item.Gender) {
+      html += `<td>${item.Gender}</td>`;
+    } else {
+      html += `<td></td>`;
+    }
+    html += `</tr>`;
+  });
+
+  html += `
+      </tbody>
+    </table>
+  `;
+
+  return html;
 }
